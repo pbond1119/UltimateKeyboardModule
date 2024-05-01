ed89feca2ec6a16501c65edc2d935b50a88ce8bc
pbond1119 <89422290+pbond1119@users.noreply.github.com>
2024-04-30 21:25:11 -0400

    updates
    having issue where key will be held down randomly and cannot figure out why

--- synth.js
+++ synth.js
@@ -1,73 +1,37 @@
-// impliment ontap feature for mobile use if time allows, CUT OUT SECOND OSCILLATOR
-
-// Importing the Keyboard and Arpeggiator sdfg
-import { setupSynthKeyboard } from "./keyboard.js";
-import { Arpeggiator, MidiManager } from "./Arpeggiator.js";
-// import { musicTools } from "./MusicTools.js";
-
-// Enable WebMidi API and handle any errors if it fails to enable.
-// This is necessary to work with MIDI devices in the web browser.
-await WebMidi.enable();
-
-// Initialize variables to store the first MIDI input and output devices detected.
-// These devices can be used to send or receive MIDI messages.
-let myInput = WebMidi.inputs[0];
-
 // Create our Audio Context
 const siteAudio = new AudioContext();
 
-// grabbing HTML objects for external keyboard and volume
-let dropInsIO = document.getElementById("dropdown-insIO");
-const gainControl = document.getElementById("gainSlider");
 
+/**
+ * Container for ADSR values that are being used by the synth
+ * @param {number} attack / updated by the updateattack function
+ * @param {number} decay / updated by the updateattack function
+ * @param {number} sustain / updated by the updatesustain function
+ * @param {number} release / updated by the updaterelease funciton
+ */
 const adsr1 = {
-  attack: 0.02,
-  decay: 0.5,
-  sustain: 0.17,
-  release: 0.5,
+  attack: 0.00,
+  decay: 0.0,
+  sustain: 0.0,
+  release: 0.0,
 };
 
+/**
+ * keeps track of the notes that actively being played so that the playkey and stopkey function
+ */
 const activeVoices = new Map();
 
 // HTML objects for Oscilattor 1
-const asdrFolder1 = document.getElementById("asdrFolder1");
+const gainControl = document.getElementById("gainSlider")
 const attackControl1 = document.getElementById("attackSlider1");
 const decayControl1 = document.getElementById("decaySlider1");
 const sustainControl1 = document.getElementById("sustainSlider1");
 const releaseControl1 = document.getElementById("releaseSlider1");
-const transposeControl1 = document.getElementById("transposeSlider1");
 const waveShape1 = document.getElementById("waveShape1");
 
-// HTML objects for Chord Select Module
-const chordFolder = document.getElementById("chordFolder");
-const chordList = document.getElementById("chordList");
-const offChord = document.getElementById("offChord");
-const majChord = document.getElementById("majChord");
-const minChord = document.getElementById("minChord");
-const augChord = document.getElementById("augChord");
-const dimChord = document.getElementById("dimChord");
-const maj7Chord = document.getElementById("maj7Chord");
-const min7Chord = document.getElementById("min7Chord");
-const dom7Chord = document.getElementById("dom7Chord");
 
-// HTML objects for Note Repeat
-const repeatFolder = document.getElementById("repeatFolder");
-const repeatDropdown = document.getElementById("repeatDropdown");
 
-// HTML objects for Arpeggiator
-const arpFolder = document.getElementById("arpFolder");
-const bpmValue = document.getElementById("bpmValue");
-const bpmSet = document.getElementById("bpmSet");
-const subdiv = document.getElementById("subdiv");
-const noteLengthValue = document.getElementById("noteLengthValue");
-
-// For each MIDI input device detected, add an option to the input devices dropdown.
-// This loop iterates over all detected input devices, adding them to the dropdown.
-WebMidi.inputs.forEach(function (input, num) {
-  dropInsIO.innerHTML += `<option value=${num}>${input.name}</option>`;
-});
-
-// GETTING HTML FOR KEYS
+// GETTING HTML FOR KEYS; JS AND HTML CODE MODIFIED BASED ON THIS CODE CODE FROM https://css-tricks.com/how-to-code-a-playable-synth-keyboard/
 const getElementByNote = (note) =>
   note && document.querySelector(`[note="${note}"]`);
 
@@ -150,12 +114,14 @@ const getHz = (note = "A", octave = 4) => {
   return A4 * Math.pow(2, N / 12);
 };
 
+// establish oscillator and gain nodes + connecting them to site Audio
 const osc1 = siteAudio.createOscillator();
 const noteADSR1 = siteAudio.createGain();
 const siteGain = siteAudio.createGain();
-
 siteGain.connect(siteAudio.destination);
+siteGain.gain.value = 0
 
+// setting up a map to keep track of the pressed keys
 const pressedNotes = new Map();
 let clickedKey = "";
 
@@ -167,13 +133,15 @@ const playKey = function (key) {
 
   // Check if the key is already pressed
   if (pressedNotes.has(key)) {
-    return;
+    return
+    stopKey(key);
   }
   let keyGrab = keys[key];
   const osc1 = siteAudio.createOscillator();
   const noteADSR1 = siteAudio.createGain();
   noteADSR1.gain.setValueAtTime(0, siteAudio.currentTime);
 
+  // making the oscillator and making sure it is connected when a key is played
   osc1.connect(noteADSR1);
   noteADSR1.connect(siteGain);
   console.log(key.note);
@@ -181,27 +149,7 @@ const playKey = function (key) {
   activeVoices.set(keyGrab.note, { osc1, noteADSR1 });
   console.log("playKey", activeVoices);
 
-  // setAttack1 = () =>
-  //   noteADSR1.gain.linearRampToValueAtTime(
-  //     1,
-  //     siteAudio.currentTime + adsr1.attack
-  //   );
-  // setDecay1 = () =>
-  //   noteADSR1.gain.linearRampToValueAtTime(
-  //     adsr1.sustain,
-  //     siteAudio.currentTime + adsr1.attack + adsr1.decay
-  //   );
-  // // const setSustain1 = () =>
-  // //   noteADSR1.gain.linearRampToValueAtTime(
-  // //     adsr1.sustain,
-  // //     zeroGain + parseFloat(sustainControl1.value)
-  // //   );
-  // setRelease1 = () =>
-  //   noteADSR1.gain.linearRampToValueAtTime(
-  //     0,
-  //     siteAudio.currentTime + adsr1.release
-  //   );
-
+// wave shape selecting function
   let setWaveShape1 = function () {
     let selectedWaveShape1 = waveShape1.value;
     osc1.type = selectedWaveShape1;
@@ -215,28 +163,37 @@ const playKey = function (key) {
 
   keys[key].element.classList.add("pressed");
   pressedNotes.set(key, osc1);
+
+  // starting the oscillator and updating the attack, sustain, and decay times
   osc1.start();
   noteADSR1.gain.linearRampToValueAtTime(
     1,
     siteAudio.currentTime + adsr1.attack
   );
   noteADSR1.gain.linearRampToValueAtTime(
+    0,
+    siteAudio.currentTime + adsr1.decay);
+  noteADSR1.gain.linearRampToValueAtTime(
     adsr1.sustain,
     siteAudio.currentTime + adsr1.attack + adsr1.decay
   );
-  console.log(adsr1.attack, adsr1.decay, adsr1.sustain);
+ 
+  console.log(adsr1.attack, adsr1.decay, adsr1.sustain, adsr1.release);
 
-  siteAudio.resume;
+  siteAudio.resume();
 
   setWaveShape1();
+
+  console.log(keyGrab)
 };
 
 setAttack1();
 setDecay1();
-// setSustain1();
+setSustain1();
 setRelease1();
 setWaveShape1();
 
+// responsible for stopping the key
 const stopKey = function (key) {
   if (!keys[key]) {
     return;
@@ -247,23 +204,34 @@ const stopKey = function (key) {
   const voice = activeVoices.get(keyGrab.note);
   console.log(voice);
 
+if (voice && voice.noteADSR1){  
   voice.noteADSR1.gain.linearRampToValueAtTime(
     0,
     siteAudio.currentTime + adsr1.release
   );
 
+  // remove pressed key
   keys[key].element.classList.remove("pressed");
+  console.log(keys)
+  // get the oscillator assosciated with the key
   const osc1 = pressedNotes.get(key);
   setTimeout(() => {
+ 
+
     voice.osc1.stop();
     voice.osc1.disconnect();
-
     activeVoices.delete(key);
-  }, adsr1.release * 1000);
+    pressedNotes.delete(key);
+    console.log(keyGrab)
+  },
+   (adsr1.release * 1000));
 
-  pressedNotes.delete(key);
+  // pressedNotes.delete(key);
+  // activeVoices.delete(key)
+}
 };
 
+// event listener for when a key is pressed, triggers the playkey function
 document.addEventListener("keydown", (e) => {
   const eventKey = e.key.toUpperCase();
   const key = eventKey === ";" ? "semicolon" : eventKey;
@@ -274,6 +242,7 @@ document.addEventListener("keydown", (e) => {
   playKey(key);
 });
 
+// event listener for when a key is release, triggers the stopkey function
 document.addEventListener("keyup", (e) => {
   const eventKey = e.key.toUpperCase();
   const key = eventKey === ";" ? "semicolon" : eventKey;
@@ -284,6 +253,7 @@ document.addEventListener("keyup", (e) => {
   stopKey(key);
 });
 
+// triggers playkey based on mouseclick
 for (const [key, { element }] of Object.entries(keys)) {
   element.addEventListener("mousedown", () => {
     playKey(key);
@@ -291,10 +261,13 @@ for (const [key, { element }] of Object.entries(keys)) {
   });
 }
 
+// triggers playkey based on mouse release
 document.addEventListener("mouseup", () => {
   stopKey(clickedKey);
 });
 
+
+// updating display and values for ADSR
 const updateAttackDisplay = function () {
   let sliderVal = parseFloat(attackControl1.value);
   adsr1.attack = sliderVal / 1000;
@@ -302,12 +275,7 @@ const updateAttackDisplay = function () {
   document.getElementById("attackDisplay1").innerText = `${sliderVal} ms`;
 };
 
-// const updateAttackValue = function () {
-//   adsr1.attack = parseFloat(attackControl1.value / 1000);
-//   console.log(adsr1.attack.value);
-//   console.log(attackControl1.value);
-// };
-
+// update values and displays
 const updateAttackValue = function (adsr1) {
   adsr1.attack = parseFloat(attackControl1.value / 1000);
 };
@@ -317,10 +285,8 @@ const updateDecayDisplay = function () {
   document.getElementById("decayDisplay1").innerText = `${sliderVal} ms`;
 };
 
-const updateDecayValue = function () {
+const updateDecayValue = function (adsr1) {
   adsr1.decay = parseFloat(decayControl1.value / 1000);
-  console.log(adsr1.decay.value);
-  console.log(decayControl1.value);
 };
 
 const updateSustainDisplay = function () {
@@ -329,10 +295,8 @@ const updateSustainDisplay = function () {
   document.getElementById("sustainDisplay1").innerText = `${sliderVal} dB`;
 };
 
-const updateSustainValue = function () {
+const updateSustainValue = function (adsr1) {
   adsr1.sustain = parseFloat(Math.pow(10, sustainControl1.value / 20));
-  console.log(adsr1.sustain.value);
-  console.log(sustainControl1.value);
 };
 
 const updateReleaseDisplay = function () {
@@ -340,109 +304,38 @@ const updateReleaseDisplay = function () {
   document.getElementById("releaseDisplay1").innerText = `${sliderVal} ms`;
 };
 
-const updateReleaseValue = function () {
+const updateReleaseValue = function (adsr1) {
   adsr1.release = parseFloat(releaseControl1.value / 1000);
-  console.log(adsr1.release.value);
-  console.log(releaseControl1.value);
+
 };
 
-const updateTranspose1 = function () {
-  let sliderVal = parseFloat(transposeControl1.value);
-  document.getElementById(
-    "transposeDisplay1"
-  ).innerText = `${sliderVal} semitones`;
-};
 
-// updateASDRFolder1();
+// gain slider;
 
-const updateGain = function () {
-  siteAudio.resume();
-  let sliderVal = parseFloat(gainControl.value);
-  document.getElementById("gainDisplay").innerText = `${sliderVal} dBFS`;
-  let linAmp = 10 ** (sliderVal / 20);
-  gainControl.gain.setValueAtTime(linAmp, siteAudio.currentTime);
-};
+const updateGain = function() {
+  siteAudio.resume()
+  let sliderVal = parseFloat(gainControl.value)
+  document.getElementById("gainDisplay").innerText = `${sliderVal} dBFS`
+  let linAmp = 10**(sliderVal/20)
+  console.log(sliderVal, linAmp)
+  siteGain.gain.setValueAtTime(linAmp, siteAudio.currentTime);
+  console.log(siteGain.gain.value)
+}
 
-// CHORD BUILDER
-document.getElementById("offChord").addEventListener("change", function () {
-  console.log(this.checked);
-  if (this.checked) {
-    console.log("Chord Build Off");
-  }
-});
 
-document.getElementById("majChord").addEventListener("change", function () {
-  console.log(this.checked);
-  if (this.checked) {
-    console.log("Chord Build = Major");
-  }
-});
-
-document.getElementById("minChord").addEventListener("change", function () {
-  console.log(this.checked);
-  if (this.checked) {
-    console.log("Chord Build = Minor");
-  }
-});
-
-document.getElementById("augChord").addEventListener("change", function () {
-  console.log(this.checked);
-  if (this.checked) {
-    console.log("Chord Build = Augmented");
-  }
-});
-
-document.getElementById("dimChord").addEventListener("change", function () {
-  console.log(this.checked);
-  if (this.checked) {
-    console.log("Chord Build = Diminished");
-  }
-});
-
-document.getElementById("maj7Chord").addEventListener("change", function () {
-  console.log(this.checked);
-  if (this.checked) {
-    console.log("Chord Build = Major 7");
-  }
-});
-
-document.getElementById("min7Chord").addEventListener("change", function () {
-  console.log(this.checked);
-  if (this.checked) {
-    console.log("Chord Build = Minor 7");
-  }
-});
-
-document.getElementById("dom7Chord").addEventListener("change", function () {
-  console.log(this.checked);
-  if (this.checked) {
-    console.log("Chord Build = Dominant 7");
-  }
-});
-
-// EVENT LISTENERS
-// attackControl1.addEventListener("input", updateASDRFolder1Text);
-// releaseControl1.addEventListener("input", updateASDRFolder1Text);
-// sustainControl1.addEventListener("input", updateASDRFolder1Text);
-// decayControl1.addEventListener("input", updateASDRFolder1Text);
-// transposeControl1.addEventListener("input", updateASDRFolder1Text);
-// waveShape1.addEventListener("change", updateASDRFolder1Text);
-
+// control event listeners
 gainControl.addEventListener("input", updateGain);
 
 attackControl1.addEventListener("input", updateAttackDisplay);
-attackControl1.addEventListener("value", updateAttackValue);
+attackControl1.addEventListener("change", updateAttackValue);
 decayControl1.addEventListener("input", updateDecayDisplay);
-decayControl1.addEventListener("value", updateDecayValue);
+decayControl1.addEventListener("input", function() {
+  updateDecayValue(adsr1);});
 
 sustainControl1.addEventListener("input", updateSustainDisplay);
-sustainControl1.addEventListener("value", updateSustainValue);
+sustainControl1.addEventListener("input", function() {
+  updateSustainValue(adsr1);});
 releaseControl1.addEventListener("input", updateReleaseDisplay);
-releaseControl1.addEventListener("value", updateReleaseValue);
-transposeControl1.addEventListener("input", updateTranspose1);
-// waveShape1.addEventListener("change", setWaveShape1);
+releaseControl1.addEventListener("input", function() {
+  updateReleaseValue(adsr1);});
 
-updateAttackValue(adsr1);
-updateDecayValue(adsr1);
-updateSustainValue(adsr1);
-updateReleaseValue(adsr1);
